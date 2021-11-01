785b50f85125ad4d1b46112ce299d0c5484dbbc3
Deepi Garg <deepigarg00@gmail.com>
2021-11-01 11:29:20 +0530

    Add shop struct

--- contracts/ClarityInCharity.sol
+++ contracts/ClarityInCharity.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 contract  ClarityInCharity{
     uint public projectCount = 0;
     uint public donorCount = 0;
+    uint public shopCount = 0;
 
     struct Project{
         uint projectID;
@@ -21,8 +22,16 @@ contract  ClarityInCharity{
         address Address;
     }
 
+    struct Shop{
+        uint shopID;
+        string name;
+        uint DAPPtokenBalance;
+        address Address;
+    }
+
     mapping(uint => Project) public projects;
     mapping(uint => Donor) public donors;
+    mapping(uint => Shop) public shops;
 
     constructor() public {
         
@@ -49,6 +58,16 @@ contract  ClarityInCharity{
             completed:false});
         projectCount++;
         projects[projectCount] = d;
+    }
+
+    function createShop(string memory myName) public {
+        Shop memory s = Shop(
+            { shopID:shopCount+1,
+            name:myName, 
+            DAPPtokenBalance:0,
+            Address:msg.sender });
+        shopCount++;
+        shops[shopCount] = s;
     }
 
     function sendMoney(uint projectID, uint donorID, uint amount) public {
